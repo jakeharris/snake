2f30035d83d9cf52d8c43d48427b5dba8aeeb970
Jake <javakatdesign@gmail.com>
2013-09-03 01:45:51 -0500

    Add snake.js and snake.css

---
+++ snake.js
@@ -0,0 +1,30 @@
+window.onload( function () {
+  
+var width = 320,
+    height = 500,
+    c = document.getElementById('c');
+
+alert(c);
+
+var ctx = c.getContext('2d'),
+    game;
+
+c.width = width;
+c.height = height;
+
+var clear = function () {
+        ctx.fillStyle = '#d0e7f9';
+        ctx.beginPath();
+        ctx.rect(0, 0, width, height);
+        ctx.closePath();
+        ctx.fill();
+    };
+
+var loop = function () {
+        clear();
+        game = setTimeout(loop, 1000 / 50);
+    };
+
+loop();
+  
+});
