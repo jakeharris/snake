b75931fecbf2173d12cd8a8756bde1e3f9ba4f49
Jake <javakatdesign@gmail.com>
2013-09-04 23:15:58 -0500

    Get to drawing! Add events.js and types.js.

--- assets/javascripts/snake.js
+++ assets/javascripts/snake.js
@@ -1,5 +1,5 @@
-var width = 320,
-    height = 500,
+var width = document.width,
+    height = document.height,
     c = document.getElementById('c'),
     ctx = c.getContext('2d'),
     game;
@@ -8,6 +8,10 @@ c.width = width;
 c.height = height;
 
 var clear = function () {
+        if(width != document.width || height != document.height) {
+          c.width = (width = document.width);
+          c.height = (height = document.height);
+        }
         ctx.fillStyle = '#d0e7f9';
         ctx.beginPath();
         ctx.rect(0, 0, width, height);
